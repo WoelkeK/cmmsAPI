4a8f37c898cb8e0695e27ce5bb2fcd7c5a2cb114
krzysztof.woelke <krzysiek@woelke.pl>
2023-02-07 08:56:24 +0100

    Add job mapper and exception handler

--- src/main/java/pl/medos/cmmsApi/service/JobService.java
+++ src/main/java/pl/medos/cmmsApi/service/JobService.java
@@ -1,69 +1,78 @@
-//package pl.medos.cmmsApi.service;
-//
-//import org.springframework.stereotype.Service;
-//import pl.medos.cmmsApi.model.Job;
-//import pl.medos.cmmsApi.repository.JobRepository;
-//
-//import java.util.List;
-//import java.util.logging.Logger;
-//
-//@Service
-//public class JobService {
-//
-//    private static final Logger LOGGER = Logger.getLogger(JobService.class.getName());
-//
-//    private JobRepository jobRepository;
-//
-//    public JobService(JobRepository jobRepository) {
-//        this.jobRepository = jobRepository;
-//    }
-//
-//    public List list() {
-//        LOGGER.info("list()");
-//        List<Job> jobList = jobRepository.findAll();
-//        LOGGER.info("List(...)");
-//        return jobList;
-//
-//    }
-//
-//    public Job create(Job job) {
-//        LOGGER.info("create(" + job + ")");
-//       Job createdJob = jobRepository.save(job);
-//        LOGGER.info("create(...)");
-//        return createdJob;
-//
-//    }
-//
-//    public Job read(Long id) {
-//        LOGGER.info("read()");
-//        Job readedJob = jobRepository.findById(id).orElseThrow();
-//        LOGGER.info("read(...)");
-//        return readedJob;
-//
-//    }
-//
-//    public Job update(Job job) {
-//        LOGGER.info("update()");
-//        Job editedJob= jobRepository.findById(job.getId()).orElseThrow();
-//        editedJob.setRequestTime(job.getRequestTime());
-//        editedJob.setUser(job.getUser());
-//        editedJob.setDepartment(job.getDepartment());
-//        editedJob.setMachine(job.getMachine());
-//        editedJob.setMessage(job.getMessage());
-//        editedJob.setDirectContact(job.getDirectContact());
-//        editedJob.setSolution(job.getSolution());
-//        editedJob.setJobStartTime(job.getJobStartTime());
-//        editedJob.setJobStopTime(job.getJobStopTime());
-//
-//        Job updatedJob = jobRepository.save(editedJob);
-//        LOGGER.info("update(...) " + updatedJob);
-//        return updatedJob;
-//    }
-//
-//    public String delete(Long id) {
-//        LOGGER.info("delete()");
-//        jobRepository.deleteById(id);
-//        LOGGER.info("delete(...)");
-//        return "Record " + id + " deleted!";
-//    }
-//}
+package pl.medos.cmmsApi.service;
+
+import org.springframework.stereotype.Service;
+import pl.medos.cmmsApi.exception.JobNotFoundException;
+import pl.medos.cmmsApi.model.Job;
+import pl.medos.cmmsApi.repository.JobRepository;
+import pl.medos.cmmsApi.repository.entity.JobEntity;
+import pl.medos.cmmsApi.service.mapper.JobMapper;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+@Service
+public class JobService {
+
+    private static final Logger LOGGER = Logger.getLogger(JobService.class.getName());
+
+    private JobRepository jobRepository;
+    private JobMapper jobMapper;
+
+    public JobService(JobRepository jobRepository, JobMapper jobMapper) {
+        this.jobRepository = jobRepository;
+        this.jobMapper = jobMapper;
+    }
+
+    public JobService(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+    public List list() {
+
+        LOGGER.info("list()");
+        List<JobEntity> jobEntities = jobRepository.findAll();
+        List<Job> jobModels = jobMapper.listModels(jobEntities);
+        LOGGER.info("List(...)");
+        return jobModels;
+    }
+
+    public Job create(Job job) {
+
+        LOGGER.info("create(" + job + ")");
+        JobEntity jobEntity = jobMapper.modelToEntity(job);
+        JobEntity savedJobEntity = jobRepository.save(jobEntity);
+        Job savedJobModel = jobMapper.entityToModel(savedJobEntity);
+        LOGGER.info("create(...)");
+        return savedJobModel;
+    }
+
+    public Job read(Long id) throws JobNotFoundException {
+
+        LOGGER.info("read()");
+        Optional<JobEntity> optionalJobEntity = jobRepository.findById(id);
+        JobEntity jobEntity = optionalJobEntity.orElseThrow(
+                () -> new JobNotFoundException("Brak zlecenia o podanym id" + id));
+        Job jobModel = jobMapper.entityToModel(jobEntity);
+        LOGGER.info("read(...)" + jobModel);
+        return jobModel;
+    }
+
+    public Job update(Job job) {
+
+        LOGGER.info("update()" + job);
+        JobEntity jobEntity = jobMapper.modelToEntity(job);
+        JobEntity updatedJobEntity = jobRepository.save(jobEntity);
+        Job updatedJobModel = jobMapper.entityToModel(updatedJobEntity);
+        LOGGER.info("update(...) " + updatedJobModel);
+        return updatedJobModel;
+    }
+
+    public String delete(Long id) {
+
+        LOGGER.info("delete()");
+        jobRepository.deleteById(id);
+        LOGGER.info("delete(...)");
+        return "Response: 200 Record" + id + " deleted!";
+    }
+}
