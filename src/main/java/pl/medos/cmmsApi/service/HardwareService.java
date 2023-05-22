3f44b269c3032c806a9f9cdeaff055ce6ee301fb
krzysztof.woelke <krzysiek@woelke.pl>
2023-05-22 11:26:39 +0200

    add it_hardware layers controller/service/repository

--- src/main/java/pl/medos/cmmsApi/service/HardwareService.java
+++ src/main/java/pl/medos/cmmsApi/service/HardwareService.java
@@ -1,12 +1,18 @@
 package pl.medos.cmmsApi.service;
 
+import org.springframework.data.domain.Page;
+import pl.medos.cmmsApi.exception.HardwareNotFoundException;
 import pl.medos.cmmsApi.model.Hardware;
+
+import java.util.List;
 
 public interface HardwareService {
 
-    void create(Hardware hardware);
-    void read(Long id);
-    void update(Hardware hardware);
+    Hardware create(Hardware hardware);
+    Hardware read(Long id) throws HardwareNotFoundException;
+    Hardware update(Hardware hardware) throws HardwareNotFoundException;
     void delete(Long id);
+    List<Hardware> listAll();
+    Page<Hardware> pagesHardware(int pageNo, int size);
 
 }
