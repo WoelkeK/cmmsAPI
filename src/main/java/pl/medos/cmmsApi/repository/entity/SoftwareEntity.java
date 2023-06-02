64427e2c94d81d7c916d57e8dae1f54af34b161e
krzysztof.woelke <krzysiek@woelke.pl>
2023-06-02 11:29:37 +0200

    add software controller, service, repo. TODO: Import / Export

--- src/main/java/pl/medos/cmmsApi/repository/entity/SoftwareEntity.java
+++ src/main/java/pl/medos/cmmsApi/repository/entity/SoftwareEntity.java
@@ -1,26 +1,27 @@
-package pl.medos.cmmsApi.model;
+package pl.medos.cmmsApi.repository.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Data
-@Builder
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Software {
-
+@Builder
+@Entity
+@Table(name = "SOFTWARES")
+public class SoftwareEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String number;
     private String version;
-    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate activateDate;
-
 
 }
