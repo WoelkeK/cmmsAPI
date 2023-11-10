1cbcd9660f5b3d62e885ffaca597195541552ee9
Krzysztof <krzysztof@woelke.pl>
2023-11-10 10:58:28 +0100

    revert changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/main/resources/templates/**/*.{html,js}"],
+  content: [
+  "./src/main/resources/templates/**/*.html"
+  ],
   theme: {
     extend: {},
   },
