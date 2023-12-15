cd2a2fd3e828857834c672c8ec48cb76e0d1d82b
Krzysztof <krzysztof@woelke.pl>
2023-12-15 14:25:54 +0100

    fix CustomIPCheckFilter null on getPermission() from db

--- src/main/java/pl/medos/cmmsApi/config/CustomIPCheckFilter.java
+++ src/main/java/pl/medos/cmmsApi/config/CustomIPCheckFilter.java
@@ -5,15 +5,19 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.xml.bind.annotation.XmlType;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
+import pl.medos.cmmsApi.enums.Permission;
 import pl.medos.cmmsApi.model.Hardware;
 import pl.medos.cmmsApi.service.HardwareService;
 
 
 import java.io.IOException;
+import java.io.InvalidObjectException;
 
 @Component
 @AllArgsConstructor
@@ -36,21 +40,28 @@ public class CustomIPCheckFilter extends GenericFilterBean {
         log.info("IP Remote address: " + remoteIP);
 
         String requestURI = request.getRequestURI();
-        log.info("URI: " + requestURI);
-
         Hardware ipAddressRole = hardwareService.findByIpAddress(remoteIP);
-        log.info("roleFromDb: " + ipAddressRole.getIpAddress()+ " " + ipAddressRole.getRole());
         req.setAttribute("isAdmin", false);
 
         if (ipAddressRole == null) {
             log.info("No match found in the repository for IP: " + remoteIP);
             throw new ServletException("Unauthorized IP Access");
         }
+        Permission permission = ipAddressRole.getPermission();
+        if (permission == null) {
+            log.info("No permission sets in the repository for IP: " + remoteIP + " default: USER");
+            permission = Permission.USER;
+            log.info("Permission "+ permission.toString());
+            ipAddressRole.setPermission(permission);
+        }
 
         if (ipAddressRole.getPermission().toString().equalsIgnoreCase("admin")) {
             req.setAttribute("isAdmin", true);
-        }
 
+        } else {
+            req.setAttribute("isAdmin", false);
+        }
+        req.setAttribute("userIP", remoteIP);
         chain.doFilter(request, res);
     }
 }
--- src/main/java/pl/medos/cmmsApi/enums/Permission.java
+++ src/main/java/pl/medos/cmmsApi/enums/Permission.java
@@ -1,6 +1,6 @@
 package pl.medos.cmmsApi.enums;
 
 public enum Permission {
-     USER, ADMIN
+    USER, ADMIN;
 }
 
