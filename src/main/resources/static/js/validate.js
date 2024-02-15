7f2f95e625938d554c2e27b67ce05eb6f6f4cfca
Krzysztof <krzysztof@woelke.pl>
2024-02-15 07:59:51 +0100

    fix validate method

--- src/main/resources/static/js/validate.js
+++ src/main/resources/static/js/validate.js
@@ -1,26 +1,17 @@
 document.querySelector('form').addEventListener('submit', function(event) {
-    var machineSelect = document.getElementById('machineId');
-    var departmentSelect = document.getElementById('departmentId');
-    var employeeSelect = document.getElementById('employeeId');
-    var errorElement1 = document.getElementById('error1');
-    var errorElement2 = document.getElementById('error2');
 
-    if (machineSelect.value == "") {
-        // Prevent the form from being submitted
-        event.preventDefault();
-        // Display error message
-        errorElement1.textContent = "Proszę wybrać urządzenie.";
-    } else {
-        // Clear any error message
-        errorElement1.textContent = '';
+    let fields = document.getElementsByClassName('validate');
+    let errors = document.getElementsByClassName('error');
+
+    for(let i=0; i<fields.length; i++){
+
+    if(fields[i].value ==""){
+         event.preventDefault();
+            // Display error message
+            errors[i].textContent = "Należy uzupełnić, pole wymagane!.";
+        } else {
+            // Clear any error message
+            errors[i].textContent = '';
     }
-    if (departmentSelect.value == "") {
-        // Prevent the form from being submitted
-        event.preventDefault();
-        // Display error message
-        errorElement2.textContent = "Proszę wybrać wydział.";
-    } else {
-        // Clear any error message
-        errorElement2.textContent = '';
     }
 });
